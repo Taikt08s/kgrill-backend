5ed0097c56ecb3ce30403b1d7ccadd00e44de823
Styematic <styematic@gmail.com>
2024-05-29 13:01:38 +0700

    update api doc

--- src/main/java/com/group2/kgrill/dto/AuthenticationResponse.java
+++ src/main/java/com/group2/kgrill/dto/AuthenticationResponse.java
@@ -13,11 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "Response object for user sign in")
 public class AuthenticationResponse {
-    @Schema(description = "Access Token", example = "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjp7InJvbGVJZCI6MSwicm9sZU5hbWUiOiJVU0VSIiwiY3JlYXRlZERhdGUiOjE3MTY5MTQ5MTEwMDAsImxhc3RNb2RpZmllZERhdGUiOjE3MTY5MTQ5MTIwMDB9LCJmdWxsTmFtZSI6IkRhbmcgRGluaCBUYWkiLCJzdWIiOiJzdHllbWF0aWNAZ21haWwuY29tIiwiaWF0IjoxNzE2OTU2NDAzLCJleHAiOjE3MTY5NTgyMDMsImF1dGhvcml0aWVzIjpbIlVTRVIiXX0.kbA1vVt5AyocVTX1YCv_oBVuuPdiiiKYEVd-9NzZiPyNS48YTOGGjdIzTotQUkv3wEzGWACjtxKx1tSWOIOHKA")
+    @Schema(description = "Access Token", example = "xxx.yyy.zzz")
     @JsonProperty("accessToken")
     private String accessToken;
 
-    @Schema(description = "Refresh Token", example = "")
+    @Schema(description = "Refresh Token", example = "xxx.yyy.zzz")
     @JsonProperty("refreshToken")
     private String refreshToken;
 }
