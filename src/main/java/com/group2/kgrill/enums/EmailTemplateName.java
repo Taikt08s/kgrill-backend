623e30f953158122c0902c5e8441502abbde136b
Styematic <styematic@gmail.com>
2024-06-04 18:42:36 +0700

    deploy hotfix email template

--- src/main/java/com/group2/kgrill/enums/EmailTemplateName.java
+++ src/main/java/com/group2/kgrill/enums/EmailTemplateName.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum EmailTemplateName {
-    ACTIVATE_ACCOUNT("activate-account");
+    ACTIVATE_ACCOUNT("ACTIVATE_ACCOUNT");
     private final String name;
 
     EmailTemplateName(String name) {
