8fd99373405f8de858daf81e93193f1cd4a20ae9
Styematic <styematic@gmail.com>
2024-06-07 21:32:53 +0700

    update files

--- src/main/java/com/group2/kgrill/KgrillApplication.java
+++ src/main/java/com/group2/kgrill/KgrillApplication.java
@@ -2,15 +2,11 @@ package com.group2.kgrill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableJpaRepositories(basePackages = "com.swd392.group2.kgrill_model.repository")
-@EntityScan(basePackages = "com.swd392.group2.kgrill_model.model")
 @ComponentScan(basePackages = {"com.group2.kgrill", "com.swd392.group2.kgrill_model", "com.swd392.group2.kgrill_dto", "com.swd392.group2.kgrill_service"})
 public class KgrillApplication {
 
