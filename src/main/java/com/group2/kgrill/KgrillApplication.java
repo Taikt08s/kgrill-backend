34911a008954e1498a7eeeb0aa8fde0d645e13e5
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-06-30 08:25:44 +0700

    add delete token method

--- src/main/java/com/group2/kgrill/KgrillApplication.java
+++ src/main/java/com/group2/kgrill/KgrillApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {"com.group2.kgrill", "com.swd392.group2.kgrill_service"})
 public class KgrillApplication {
 
