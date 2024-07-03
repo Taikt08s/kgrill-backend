9e69d95c2effa1fcc9051a32a645d5dfc0ae9442
Styematic <styematic@gmail.com>
2024-07-03 20:30:40 +0700

    update main

--- src/main/java/com/group2/kgrill/KgrillApplication.java
+++ src/main/java/com/group2/kgrill/KgrillApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.group2.kgrill", "com.swd392.group2.kgrill_service"})
+@ComponentScan(basePackages = {"com.swd392.group2.kgrill_service"})
 public class KgrillApplication {
 
     public static void main(String[] args) {
