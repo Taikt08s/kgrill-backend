011791ed7a0503c1a153469ba3e4017c9de86273
Nope2709 <duongdhse173027@fpt.edu.vn>
2024-07-09 12:52:14 +0700

    update Dish controller

--- src/main/java/com/group2/kgrill/controller/DishController.java
+++ src/main/java/com/group2/kgrill/controller/DishController.java
@@ -2,6 +2,7 @@ package com.group2.kgrill.controller;
 
 
 import com.swd392.group2.kgrill_service.dto.DishDTO;
+import com.swd392.group2.kgrill_service.dto.request.DishRequest;
 import com.swd392.group2.kgrill_service.exception.CustomSuccessHandler;
 import com.swd392.group2.kgrill_service.service.DishService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -99,8 +100,47 @@ public class DishController {
             @ApiResponse(responseCode = "400", description = "Failed to get dish"),
     })
     @GetMapping("dish/{id}")
-    public ResponseEntity<DishDTO> dish(@PathVariable int id){
+    public ResponseEntity<DishRequest> dish(@PathVariable int id){
         return ResponseEntity.ok(dishService.getDishByID(id));
+    }
+    @Operation(
+            summary = "Search dish",
+            description = "Search dish by name and price",
+            tags = {"Dish"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get dish successfully",
+                    content = @Content(
+                            examples = @ExampleObject(value = """
+                                        {
+                                           "http_status": 200,
+                                           "time_stamp": "06/02/2024 17:25:41",
+                                           "message": "Successfully retrieved dishes list information",
+                                           "data": {
+                                             "content": [
+                                               {
+                                                 "dish_id": "a9126139-3c11-47ba-8493-cf7e480c3645",
+                                                 "dish_name": "Bò Mĩ",
+                                                 "dish_price": "289999",
+                                                 }
+                                                  ],
+                                             "page_no": "0",
+                                             "page_size": "1",
+                                             "total_elements": "10",
+                                             "total_pages": "1",
+                                             "last": false,
+                                           }
+                                         }
+                                    """))),
+            @ApiResponse(responseCode = "400", description = "Failed to get dish"),
+    })
+    @GetMapping("/dish/search-by-filter")
+    public ResponseEntity<Object> searchDishByFilter(@Parameter(description = "Page number, starting from 1", required = true) @RequestParam(name = "pageNumber", defaultValue = "1") int pageNumber,
+                                                     @Parameter(description = "Page size, 10 dishes max", required = true) @RequestParam(name = "pageSize", defaultValue = "10") int pageSize,
+                                                     @Parameter(description = "Sort field default by Id", required = true) @RequestParam(name = "sortField", defaultValue = "id") String sortField,
+                                                     @Parameter(description = "Sort by ascending or descending") @RequestParam(name = "sortDir", defaultValue = "asc") String sortDir,
+//                                                              @Parameter(description = "Filter by name/size/") @RequestParam(name = "filter", required = false) String filter,
+                                                     @Parameter(description = "Search keyword")@RequestParam("keyword") String keyword){
+        return ResponseEntity.ok(dishService.searchDishByFilter(pageNumber, pageSize, sortField, sortDir, keyword));
     }
     @Operation(
             summary = "Create new dish",
@@ -135,9 +175,9 @@ public class DishController {
     })
     @PostMapping("dish/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<DishDTO> dishCreate(@RequestBody DishDTO dishDTO){
-
-        return new ResponseEntity<>(dishService.createDish(dishDTO),HttpStatus.CREATED);
+    public ResponseEntity<Object> dishCreate(@RequestBody DishRequest dishDTO){
+        dishService.createDish(dishDTO);
+        return CustomSuccessHandler.responseBuilder(HttpStatus.OK, "Create a new dish successfully", "");
     }
 
     @Operation(
@@ -149,11 +189,11 @@ public class DishController {
             @ApiResponse(responseCode = "400", description = "Failed to update dish"),
     })
 
-    @PutMapping("dish/{id}/update")
-    public ResponseEntity<Object> dishUpdate(@RequestBody DishDTO dishDTO, @PathVariable("id") int id){
+    @PutMapping("dish/update")
+    public ResponseEntity<Object> dishUpdate(@RequestBody DishRequest dishDTO){
 
-        dishService.updateDish(dishDTO, id);
-        return new ResponseEntity<>("Update dish successfully",HttpStatus.OK);
+        dishService.updateDish(dishDTO);
+        return CustomSuccessHandler.responseBuilder(HttpStatus.OK, "Updated dish successfully", "");
     }
 
     @DeleteMapping("dish/{id}/delete")
