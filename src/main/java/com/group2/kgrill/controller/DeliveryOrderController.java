08d5dd776371b463be595b13ff13b6d1166bd70a
Nope2709 <duongdhse173027@fpt.edu.vn>
2024-07-17 11:10:56 +0700

    update dish controller and cancel order

--- src/main/java/com/group2/kgrill/controller/DeliveryOrderController.java
+++ src/main/java/com/group2/kgrill/controller/DeliveryOrderController.java
@@ -1,6 +1,7 @@
 package com.group2.kgrill.controller;
 
 import com.swd392.group2.kgrill_service.dto.DeliveryLocationDTO;
+import com.swd392.group2.kgrill_service.exception.CustomSuccessHandler;
 import com.swd392.group2.kgrill_service.service.DeliveryOrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -39,6 +40,10 @@ public class DeliveryOrderController {
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to update delivery order location");
         }
+    }
+    @GetMapping("/cancel-order/{orderId}")
+    public ResponseEntity<Object> cancelOrderForManager(@PathVariable Long orderId){
+        return deliveryOrderService.cancelOrderForManager(orderId);
     }
 
 }
